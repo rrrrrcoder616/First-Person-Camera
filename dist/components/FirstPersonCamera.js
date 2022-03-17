11cea01dc59d382227c9a787dad8b99842dc2e25
Luki1235512 <Luki1235512@gmail.com>
2022-03-17 13:52:07 +0100

    added movement

--- dist/components/FirstPersonCamera.js
+++ dist/components/FirstPersonCamera.js
@@ -31,6 +31,7 @@ export class FirstPersonCamera {
     update(timeElapsedS) {
         this.updateRotation_(timeElapsedS)
         this.updateCamera_(timeElapsedS)
+        this.updateTranslation_(timeElapsedS)
 
         this.input_.update(timeElapsedS)
     }
@@ -79,6 +80,29 @@ export class FirstPersonCamera {
         }
 
         this.camera_.lookAt(closest);
+    }
+
+    updateTranslation_(timeElapsedS) {
+        const forwardVelocity = (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0)
+        const strafeVelocity = (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0)
+
+        const qx = new THREE.Quaternion()
+        qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_)
+
+        const forward = new THREE.Vector3(0, 0, -1)
+        forward.applyQuaternion(qx)
+        forward.multiplyScalar(forwardVelocity * timeElapsedS * 10)
+
+        const left = new THREE.Vector3(-1, 0, 0)
+        left.applyQuaternion(qx)
+        left.multiplyScalar(strafeVelocity * timeElapsedS * 10)
+
+        this.translation_.add(forward)
+        this.translation_.add(left)
+
+        if (forwardVelocity != 0 || strafeVelocity != 0) {
+            this.headBobActive_ = true
+        }
     }
 
 }
